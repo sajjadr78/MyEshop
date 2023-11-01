5d6211e08a36f42106b17bb859db4954f0a80eca
sajjadr78 <stajmir78@gmail.com>
2023-11-02 02:03:01 +0330

    Add project files.

---
+++ MyEshop/Migrations/20231101155526_AddFileExtensionToProductTbl.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyEshop.Migrations
+{
+    public partial class AddFileExtensionToProductTbl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UsersUserId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UsersUserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UsersUserId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileExtension",
+                table: "Products",
+                maxLength: 6,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "FileExtension",
+                value: ".jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "FileExtension",
+                value: ".jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "FileExtension",
+                value: ".jpg");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FileExtension",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsersUserId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UsersUserId",
+                table: "Orders",
+                column: "UsersUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UsersUserId",
+                table: "Orders",
+                column: "UsersUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ MyEshop/Migrations/20231101161511_init_A_UserAsAdmin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyEshop.Migrations
+{
+    public partial class init_A_UserAsAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "Email", "IsAdmin", "Password", "RegisterDate" },
+                values: new object[] { 1, "stajmir78@gmail.com", true, "123", new DateTime(2023, 11, 1, 19, 45, 11, 200, DateTimeKind.Local).AddTicks(2643) });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "OrderId", "CreateDate", "IsFinaly", "UserId" },
+                values: new object[] { 1, new DateTime(2023, 11, 1, 19, 45, 11, 201, DateTimeKind.Local).AddTicks(1477), false, 1 });
+
+            migrationBuilder.InsertData(
+                table: "OrderDetails",
+                columns: new[] { "DetailId", "Count", "OrderId", "Price", "ProductId" },
+                values: new object[] { 1, 1, 1, 555m, 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "OrderDetails",
+                keyColumn: "DetailId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1);
+        }
+    }
+}
